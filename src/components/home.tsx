1600945bc1b527512c265ae21ccdec2f3a04312c
rmil <rhys.milling@googlemail.com>
2020-07-19 21:18:19 +0100

    Homepage statistics

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,45 +1,78 @@
-import React from "react";
-import { Typography, Card, Row, Col, Calendar, Layout } from "antd";
+import React, { useState, useEffect } from "react";
+import { Typography, Card, Row, Col, Calendar, Layout, Button } from "antd";
+import { number } from "yup";
+import Axios from "axios";
+import FormatBytes from "../utils/formatBytes";
+import NumberWithCommas from "../utils/numberWithCommas";
 const { Title, Paragraph } = Typography;
 const { Content, Sider } = Layout;
 const Home = () => {
   return (
     <React.Fragment>
-      <Card title="Welcome to YSTV Creator Studio!">
-        <Paragraph>
-          From this web app you will be able to manage all aspects of an
-          exported video. Select one of the menu's to begin!
-        </Paragraph>
-      </Card>
-      <Layout>
+      <Layout className="site-layout-background">
         <Content>
-          <Calendar />
+          <Calendar mode="month" />
         </Content>
         <Sider style={{ marginLeft: 10 }} className="site-layout-background">
-          <Card title="How-to's">
+          <Card title="How-to's" style={{ marginBottom: 10 }}>
             <Paragraph>
               First time using Creator Studio? Watch our intro video or have a
               look at our documentation!
             </Paragraph>
+            <Button type="primary">Get Started</Button>
           </Card>
-          <Card title="Statistics">
-            <p>
-              Total videos: <b>100</b>
-            </p>
-            <p>
-              Total viewcount: <b>67k</b>
-            </p>
-            <p>
-              Pending videos: <b>2</b>
-            </p>
-            <p>
-              Encoding videos: <b>4</b>
-            </p>
-          </Card>
+          <StatCard />
         </Sider>
       </Layout>
     </React.Fragment>
   );
+};
+
+const StatCard = () => {
+  const [creatorStat, setCreatorStat] = useState<ICreatorStats>();
+  useEffect(() => {
+    getData();
+  });
+  interface ICreatorStats {
+    totalVideos: number;
+    totalPendingVideos: number;
+    totalVideoHits: number;
+    totalStorageUsed: number;
+  }
+  const getData = async () => {
+    await Axios.request<ICreatorStats>({
+      url: "http://localhost:8081/v1/internal/creator/stats",
+    }).then((response) => {
+      setCreatorStat(response.data);
+    });
+  };
+  if (creatorStat) {
+    return (
+      <Card title="Statistics">
+        <p>
+          Total videos
+          <br />
+          <b>{NumberWithCommas(creatorStat.totalVideos)}</b>
+        </p>
+        <p>
+          Total viewcount
+          <br />
+          <b>{NumberWithCommas(creatorStat.totalVideoHits)}</b>
+        </p>
+        <p>
+          Pending videos
+          <br />
+          <b>{creatorStat.totalPendingVideos}</b>
+        </p>
+        <p>
+          Total storage used
+          <br />
+          <b>{FormatBytes(creatorStat.totalStorageUsed)}</b>
+        </p>
+      </Card>
+    );
+  }
+  return <Card title="Statistics" loading />;
 };
 
 export default Home;
