f340358921a054fdb6f9b98d314fc32dd86bd3f8
rmil <rhys.milling@googlemail.com>
2020-09-04 22:04:05 +0100

    Updated auth to correct endpoint, more strings

--- src/types/User.ts
+++ src/types/User.ts
@@ -14,7 +14,7 @@ export interface IPermission {
 
 enum UserRoles {
   admin = "creatorAdmin",
-  superUser = "SuperUser", //Here since ystv uses this as a universal role
+  superUser = "SuperUser", // Here since ystv uses this as a universal role
   moderator = "creatorModerator",
   member = "member",
 }
@@ -22,8 +22,12 @@ enum UserRoles {
 // Some views will be for certain groups and
 // some will be available for all
 const userRoles = {
-  admin: [UserRoles.admin, UserRoles.superUser],
-  moderator: [UserRoles.moderator, UserRoles.admin, UserRoles.superUser],
+  admin: [String(UserRoles.admin), String(UserRoles.superUser)],
+  moderator: [
+    String(UserRoles.moderator),
+    String(UserRoles.admin),
+    String(UserRoles.superUser),
+  ],
   all: [
     String(UserRoles.admin),
     String(UserRoles.superUser),
