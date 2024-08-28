ce3c00716e0b85bcb9ce5b3ef247f01050034458
Liam Burnand <liam.burnand@ystv.co.uk>
2024-08-28 02:26:56 +0100

    Testing another way to do requests

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,24 +1,36 @@
-import axios, { AxiosResponse } from "axios";
+import axios, {AxiosResponse} from "axios";
 
 interface AccessToken {
-  token: string
+    token: string
 }
 
-const instance = axios.create({
-  baseURL: process.env.REACT_APP_SECURITY_BASEURL,
-  withCredentials: true,
-});
+// const instance = axios.create({
+//     baseURL: process.env.REACT_APP_SECURITY_BASEURL,
+//     withCredentials: true,
+// });
 
 const resBody = (res: AxiosResponse) => res.data;
 
-const reqs = {
-  get: (path: string) => instance.get(path).then(resBody),
-  post: (path: string, body: {}) => instance.post(path, body).then(resBody),
-  put: (path: string, body: {}) => instance.put(path, body).then(resBody),
-  delete: (path: string) => instance.delete(path).then(resBody),
-};
+// const reqs = {
+//     get: (path: string) => instance.get(path).then(resBody),
+//     post: (path: string, body: {}) => instance.post(path, body).then(resBody),
+//     put: (path: string, body: {}) => instance.put(path, body).then(resBody),
+//     delete: (path: string) => instance.delete(path).then(resBody),
+// };
 
 export const Token = {
-  getToken: (): Promise<AccessToken> => reqs.get("/api/set_token"),
-  testToken: () => reqs.get("/api/test"),
+    getToken: (): Promise<AccessToken> => axios({
+        method: "get",
+        baseURL: process.env.REACT_APP_SECURITY_BASEURL,
+        url: "/api/set_token",
+        withCredentials: true,
+    }).then(resBody),
+    testToken: () => axios({
+        method: "get",
+        baseURL: process.env.REACT_APP_SECURITY_BASEURL,
+        url: "/api/test",
+        withCredentials: true,
+    }).then(resBody),
+    // getToken: (): Promise<AccessToken> => reqs.get("/api/set_token"),
+    // testToken: () => reqs.get("/api/test"),
 };
